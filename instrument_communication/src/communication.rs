e79efd24d32c7b822cfaeed4592fa57a1d5fb2f7
Ayman Jundi <ajundi@gmail.com>
2023-09-07 22:39:18 -0400

    InstConnection trait now uses InstAddr instead of a string representation.
    Signed-off-by: Ayman Jundi <ajundi@gmail.com>

--- instrument_communication/src/communication.rs
+++ instrument_communication/src/communication.rs
@@ -1,7 +1,7 @@
-use crate::err::Error;
+use crate::{err::Error, address::InstAddr};
 
 pub trait InstConnection {
-    fn address(&self) -> &dyn AsRef<str>;
+    fn address(&self) -> InstAddr;
     fn set_timeout(&self, timeout: u64) -> Result<(), Error>;
     fn reconnect(&self) -> Result<(), Error>;
 }

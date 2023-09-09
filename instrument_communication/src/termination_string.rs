28a8871856c603b52a02d5951dc3da7e0313b5cf
Ayman Jundi <ajundi@gmail.com>
2023-09-08 22:54:58 -0400

    Added some useful trait implementations to TerminationString.
    Signed-off-by: Ayman Jundi <ajundi@gmail.com>

--- instrument_communication/src/termination_string.rs
+++ instrument_communication/src/termination_string.rs
@@ -1,9 +1,9 @@
-
-
+#[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash,Default)]
 pub enum TerminationString{
     LF,
     CR,
     CRLF,
+    #[default]
     Unkown,
     None,
     Custom(Vec<u8>)

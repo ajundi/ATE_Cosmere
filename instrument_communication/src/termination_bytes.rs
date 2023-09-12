c5af5961e3f9b17df57dae77e08bf281b53aecc9
Ayman Jundi <ajundi@gmail.com>
2023-09-12 01:17:13 -0400

    Removed Termination Bytes Unkown varient as it is ambiguous with None. If we needed to indicate that it is unkown we can use an option<TerminationBytes> set to None.
    Added translation of different TerminationBytes to a byte array.
    
    Signed-off-by: Ayman Jundi <ajundi@gmail.com>

--- instrument_communication/src/termination_bytes.rs
+++ instrument_communication/src/termination_bytes.rs
@@ -1,10 +1,21 @@
 #[derive(Clone, Debug, PartialEq, Eq, PartialOrd, Ord, Hash, Default)]
 pub enum TerminationBytes {
+    #[default]
     LF,
     CR,
     CRLF,
-    #[default]
-    Unkown,
     None,
     Custom(Vec<u8>),
+}
+
+impl TerminationBytes {
+    pub fn bytes(&self) -> &[u8] {
+        match self {
+            TerminationBytes::LF => &[10],
+            TerminationBytes::CR => &[13],
+            TerminationBytes::CRLF => &[13, 10],
+            TerminationBytes::None => &[],
+            TerminationBytes::Custom(bytes) => bytes.as_slice(),
+        }
+    }
 }
